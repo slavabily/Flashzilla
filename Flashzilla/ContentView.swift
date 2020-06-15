cdf769e626b53d31fa6500943257a96a47cac343
slavabily <slavabily@me.com>
2020-06-15 22:54:42 +0300

    Supporting specific accessibility needs with SwiftUI

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -10,30 +10,66 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var scale: CGFloat = 1
+    
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
  
     var body: some View {
+        
+        // MARK: Differentiate without color
+//        HStack {
+//            if differentiateWithoutColor {
+//                Image(systemName: "checkmark.circle")
+//            }
+//            Text("Success")
+//        }
+//        .padding()
+//        .background(differentiateWithoutColor ? Color.black : Color.green)
+//        .foregroundColor(.white)
+//        .clipShape(Capsule())
+        
+        // MARK: Reduce motion
+//        Text("Hello, World!")
+//        .scaleEffect(scale)
+////            .onTapGesture {
+////                if self.reduceMotion {
+////                    self.scale *= 1.5
+////                } else {
+////                    withAnimation {
+////                        self.scale *= 1.5
+////                    }
+////                }
+//
+//            .onTapGesture {
+//                self.withOptionalAnimation {
+//                    self.scale *= 1.5
+//                }
+        
+        // MARK: Reduce transparency
         Text("Hello, World!")
-//            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { (_) in
-//                print("Moving to the background!")
-//        }
-        
-//            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { (_) in
-//                print("Moving back to the foreground!")
-//        }
-        
-//            .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { (_) in
-//                print("User took a screen shot!")
-//        }
-        
-//            .onReceive(NotificationCenter.default.publisher(for: UIApplication.significantTimeChangeNotification)) { (_) in
-//                print("User changed their clock!")
-//        }
-        
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.keyboardDidShowNotification)) { (_) in
-                print("Keyboard is shown!")
+            .padding()
+            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+    }
+    
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
         }
     }
 }
+    
+    
+
+    
+ 
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
