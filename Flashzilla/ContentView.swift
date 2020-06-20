0508ff7f5d4e4bc36e3e48bbd5faad404d94101a
slavabily <slavabily@me.com>
2020-06-20 17:37:47 +0300

    Coloring views as we swipe

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -16,6 +16,7 @@ extension View {
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiatewithoutColor
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
     var body: some View {
@@ -32,8 +33,29 @@ struct ContentView: View {
                                 self.removeCard(at: index)
                             }
                         }
-                            .stacked(at: index, in: self.cards.count)
+                        .stacked(at: index, in: self.cards.count)
                     }
+                }
+            }
+            if differentiatewithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(Color
+                                .black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
                 }
             }
         }
