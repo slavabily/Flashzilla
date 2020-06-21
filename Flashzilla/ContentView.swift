f909f606c9edf6c883fd8ba61b58a47310591832
slavabily <slavabily@me.com>
2020-06-21 09:07:07 +0300

    Counting down with a Timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -19,6 +19,11 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiatewithoutColor
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var isActive = true
+    
     var body: some View {
         ZStack {
             Image("background")
@@ -26,6 +31,15 @@ struct ContentView: View {
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                Capsule()
+                    .fill(Color.black)
+                    .opacity(0.75))
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -58,6 +72,18 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { (time) in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { (_) in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { (_) in
+            self.isActive = true
         }
         
         
